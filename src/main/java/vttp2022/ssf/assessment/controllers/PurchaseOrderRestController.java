fbdfa4cb764c333c895cdc0be4ecbc4752979a40
w-zerita <79044966+w-zerita@users.noreply.github.com>
2022-03-25 09:26:48 +0800

    task2

---
+++ src/main/java/vttp2022/ssf/assessment/controllers/PurchaseOrderRestController.java
@@ -0,0 +1,10 @@
+package vttp2022.ssf.assessment.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/po")
+public class PurchaseOrderRestController {
+    
+}
---
+++ src/main/java/vttp2022/ssf/assessment/services/QuotationService.java
@@ -0,0 +1,8 @@
+package vttp2022.ssf.assessment.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuotationService {
+    
+}
